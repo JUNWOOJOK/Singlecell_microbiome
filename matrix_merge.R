a700dd699a908b1cea277bb42583d683850c65c7
junwoojo <78505823+JUNWOOJOK@users.noreply.github.com>
2023-04-03 08:21:09 +0900

    Update matrix_merge.R

--- matrix_merge.R
+++ matrix_merge.R
@@ -16,4 +16,33 @@ singlecell_microbiome_matrix<-rbindlist(lapply(paste0("s",1:6,"_t"),get),fill = 
 rownames(singlecell_microbiome_matrix)<-rowname_vector
 
 
+singlecell_microbiome_matrix%>%colnames()
 
+
+
+
+pbmc@meta.data
+#######################KUL####################################################################
+rowname_vector<-c()
+
+for (i in c("01","19","21","30","28")){
+  for (l in c("N","B","T")){
+  assign(paste0('K',i,l),read.table(paste0("/home/junwoojo/KUL",i,"_",l,"_matrix"),header = T))
+  zzz<-get(paste0('K',i,l))
+  colnames(zzz)<-gsub("\\.","-",colnames(get(paste0('K',i,l))))
+  colnames(zzz)<-paste0("KUL",i,"_",l,"_",colnames(zzz))
+  rowname_vector<-append(rowname_vector,zzz%>%colnames())
+  assign(paste0('K',i,l),zzz)
+  assign(paste0('K',i,l,"_t"),t(zzz)%>%as.data.frame())
+}
+}
+vector_KUL_file=c()
+for (i in c("01","19","21","30","28")){
+  for (l in c("N","B","T")){
+    vector_KUL_file<-append(vector_KUL_file,paste0("K",i,l))
+}
+}
+vector_KUL_file
+
+singlecell_microbiome_matrix<-rbindlist(lapply(paste0(vector_KUL_file,"_t"),get),fill = T)%>%as.data.frame()
+rownames(singlecell_microbiome_matrix)<-rowname_vector
